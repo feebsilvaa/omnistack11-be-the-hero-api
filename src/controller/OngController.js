a60bdcdd19c42dcf79f2d33f6519be824988211c
Fernando Barbosa <fernando.barbosa@meta.com.br>
2020-03-28 01:25:55 -0300

    incluindo validacao dos dados para criar nova ong

--- src/controller/OngController.js
+++ src/controller/OngController.js
@@ -13,6 +13,11 @@ module.exports = {
   async create (req, res) {
     console.debug(`OngController::create`);
     const { name, email, whatsapp, city, uf } = req.body;
+
+    if (!name || !email || !whatsapp || !city || !uf) 
+      return res.status(412).json({ erro: "Todos os campos são obrigatórios." });
+    
+
     const id = crypto.randomBytes(4).toString('HEX');
 
     await conn(TABLE_NAME).insert({
