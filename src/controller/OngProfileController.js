35e062e3d27c6fe167006c785dbc74a2fcc5648f
Fernando Barbosa <fernando.barbosa@meta.com.br>
2020-03-28 01:26:28 -0300

    incluindo parametro para listar todos os itens

--- src/controller/OngProfileController.js
+++ src/controller/OngProfileController.js
@@ -5,21 +5,31 @@ const INCIDENTS_TABLE = 'incidents';
 
 module.exports = {
   async listIncidents(req, res) {
-    let { page = 0, size = 5 } = req.query;
+    let { page = 0, size = 5, listAll = false } = req.query;
     console.debug(`OngProfileController::listIncidents::page=${page}size=${size}`);
     const ong_id = req.headers.authorization;
 
     if (!ong_id) {
       return res.status(400).json({ error: 'Invalid ong_id' })
     }
+    console.log(listAll);
     
-    const incidents = await conn(INCIDENTS_TABLE)
-      .where('ong_id', ong_id)
-      .limit(size)
-      .offset(page * size)
-      .select('*');
+    let incidents = []
+    if(listAll === 'true') {
+      incidents = await conn(INCIDENTS_TABLE)
+        .where('ong_id', ong_id)
+        .select('*');
+    } else {
+      incidents = await conn(INCIDENTS_TABLE)
+        .where('ong_id', ong_id)
+        .limit(size)
+        .offset(page *  size)
+        .select('*');
+    }
 
-    let [total] = await conn(INCIDENTS_TABLE).count('*');
+    let [total] = await conn(INCIDENTS_TABLE)
+      .where('ong_id', ong_id)
+      .count('*');
     const pagination = utils.getPaginationInfo(page, size, incidents, total);
 
     return res.json(utils.getPaginationPresenter(pagination, incidents));
