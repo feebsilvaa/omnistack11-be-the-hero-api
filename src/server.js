b028614fd88424aa4ea851e9dd0a5b80f903faff
Fernando Barbosa <fernando.barbosa@meta.com.br>
2020-03-31 20:54:59 -0300

    adding Procfile and setting up env port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 
+const PORT = process.env.PORT ? process.env.PORT : 3333;
+
 // escuta a porta 3333 para receber as requisições
-app.listen(3333, () => {
-  console.debug("Server is running and listening on 3333 port.")
+app.listen(PORT, () => {
+  console.log(`Lintening ${PORT}.`)
 });
